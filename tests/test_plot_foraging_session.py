18301c0187fe74d040e69d3c400ee296c1d6b833
han-lab <houhan@gmail.com>
2024-06-20 13:36:25 -0700

    test: add vertical plot

--- tests/test_plot_foraging_session.py
+++ tests/test_plot_foraging_session.py
@@ -6,37 +6,38 @@ import numpy as np
 import os
 
 from aind_dynamic_foraging_basic_analysis import plot_foraging_session
-from nwb_io import get_history_from_nwb
+from tests.nwb_io import get_history_from_nwb
 
 
 class TestPlotSession(unittest.TestCase):
     """Test plot session"""
     
-    def test_plot_session(self):
-        
+    @classmethod
+    def setUp(cls):
         nwb_file = os.path.dirname(__file__) + f"/data/697929_2024-02-22_08-38-30.nwb"
         (
             _,
-            choice_history,
-            reward_history,
-            p_reward,
-            autowater_offered,
+            cls.choice_history,
+            cls.reward_history,
+            cls.p_reward,
+            cls.autowater_offered,
             _,
         ) = get_history_from_nwb(nwb_file)
-        
+    
+    def test_plot_session(self):        
         # Plot session
-        fig, axes = plot_foraging_session(
-            choice_history=choice_history,
-            reward_history=reward_history,
-            p_reward=p_reward,
-            autowater_offered=autowater_offered,
+        fig, _ = plot_foraging_session(
+            choice_history=self.choice_history,
+            reward_history=self.reward_history,
+            p_reward=self.p_reward,
+            autowater_offered=self.autowater_offered,
             fitted_data=None, 
             photostim=None,    # trial, power, s_type
             valid_range=None,
             smooth_factor=5, 
             base_color='y', 
             ax=None, 
-            vertical=False
+            vertical=False,
         )
         
         # Save fig
@@ -45,8 +46,25 @@ class TestPlotSession(unittest.TestCase):
             bbox_inches='tight',
         )
     
+    def test_plot_session_vertical(self):        
+        # Plot session
+        fig, _ = plot_foraging_session(
+            choice_history=self.choice_history,
+            reward_history=self.reward_history,
+            p_reward=self.p_reward,
+            autowater_offered=self.autowater_offered,
+            fitted_data=None, 
+            photostim=None,    # trial, power, s_type
+            valid_range=None,
+            smooth_factor=5, 
+            base_color='y', 
+            ax=None, 
+            vertical=True,
+        )
+        
+        # Save fig
+        fig.savefig(
+            os.path.dirname(__file__) + "/data/test_plot_session_vertical.png",
+            bbox_inches='tight',
+        )
     
-if __name__ == "__main__":
-    # Run the tests in the current file only
-    suite = unittest.defaultTestLoader.loadTestsFromName(__name__)
-    unittest.TextTestRunner().run(suite)
