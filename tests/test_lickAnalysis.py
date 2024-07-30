71befc821ddfc502d2b99828ee1b8d5c6bfb7818
ZhixiaoSu <zhixiaosu@gmail.com>
2024-07-30 14:34:05 -0700

    Add read zarr

--- tests/test_lickAnalysis.py
+++ tests/test_lickAnalysis.py
@@ -36,12 +36,26 @@ class testLickPlot(unittest.TestCase):
         self.assertIsInstance(fig, plt.Figure)
         self.assertIsInstance(session_id, str)
 
-    def test_output_is_nwb(self):
-        """Test the plotLickAnalysis."""
+    def test_output_is_nwb_file(self):
+        """Test the nwb file load."""
         data_dir = Path(os.path.dirname(__file__))
         nwbfile = os.path.join(data_dir, "data/689514_2024-02-01_18-06-43.nwb")
         nwb = load_nwb(nwbfile)
         self.assertIsNotNone(nwb)
+
+    def test_output_is_nwb_folder(self):
+        """Test the nwb file load."""
+        data_dir = Path(os.path.dirname(__file__))
+        nwbfile = os.path.join(data_dir, "data/717121_2024-06-15_10-58-01.nwb")
+        nwb = load_nwb(nwbfile)
+        self.assertIsNotNone(nwb)
+
+    def test_output_is_none(self):
+        """Test the nwb file load."""
+        data_dir = Path(os.path.dirname(__file__))
+        nwbfile = os.path.join(data_dir, "data/717121_2024-06-15_10-58-00.nwb")
+        nwb = load_nwb(nwbfile)
+        self.assertIsNone(nwb)
 
     def test_lickMetrics_long(self):
         """Test lickMetrics."""
