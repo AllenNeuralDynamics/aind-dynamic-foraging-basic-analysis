0c8f417be2117054140caeb36271e7c72e306fd8
Alex Piet <alexpiet@gmail.com>
2024-08-26 16:12:35 -0700

    linting

--- src/aind_dynamic_foraging_basic_analysis/plot/plot_foraging_session.py
+++ src/aind_dynamic_foraging_basic_analysis/plot/plot_foraging_session.py
@@ -321,7 +321,7 @@ def plot_foraging_session(  # noqa: C901
     return ax_choice_reward.get_figure(), [ax_choice_reward, ax_reward_schedule]
 
 
-def plot_session_scroller(df_events, ax=None, adjust_time=True, fip_df=None):  # noqa: C901 
+def plot_session_scroller(df_events, ax=None, adjust_time=True, fip_df=None):  # noqa: C901
     """
     Creates an interactive plot of the session.
     Plots left/right licks/rewards, and go cues
