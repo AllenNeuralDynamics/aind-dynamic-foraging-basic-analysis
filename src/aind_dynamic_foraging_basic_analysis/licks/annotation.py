c0aec3f342df92a3266d77cd27482d525d8bcf5c
Alex Piet <alexpiet@gmail.com>
2024-08-29 17:41:57 -0700

    first pass annotation of bouts

---
+++ src/aind_dynamic_foraging_basic_analysis/licks/annotation.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+
+from aind_dynamic_foraging_data_utils import nwb_utils as nu
+'''
+    TODO
+    update plot_session_scroller to take NWB file
+    plot lick bouts in rotating colors
+    annotate lick bouts with reward (most recent lick, unless the lick was more than .5 seconds, or a gocue happened before)
+    annotate possible bad licks?
+    annotate cross licks?
+    use lick annotations for rewarded and unrewarded lick bout starts
+    maybe annotate rewarded and unrewarded go cues
+    Is the PSTH code flexible enough to take different options?   
+'''
+
+
+def annotate_lick_bouts(nwb, bout_threshold=.7):
+
+    if not hasattr(nwb, 'df_events'):
+        nwb.df_events = nu.create_events_df(nwb)
+    df_licks = nwb.df_events.query('event in ["right_lick_time","left_lick_time"]').copy()        
+
+    # Computing ILI for each lick 
+    df_licks['pre_ili'] = np.concatenate([
+        [np.nan],np.diff(df_licks.timestamps.values)])
+    df_licks['post_ili'] = np.concatenate([
+        np.diff(df_licks.timestamps.values),[np.nan]])
+
+    # Assign licks into bouts
+    df_licks['bout_start'] = df_licks['pre_ili'] > bout_threshold
+    df_licks['bout_end'] = df_licks['post_ili'] > bout_threshold
+    df_licks.loc[df_licks['pre_ili'].isnull(),'bout_start']=True
+    df_licks.loc[df_licks['post_ili'].isnull(),'bout_end']=True
+    df_licks['bout_number'] = np.cumsum(df_licks['bout_start'])
+
+    # Check that bouts start and stop
+    num_bout_start = df_licks['bout_start'].sum()
+    num_bout_end = df_licks['bout_end'].sum()
+    num_bouts = df_licks['bout_number'].max()
+    assert num_bout_start==num_bout_end, "Bout Starts and Bout Ends don't align"
+    assert num_bout_start == num_bouts, "Number of bouts is incorrect"
+
+    return df_licks
+
