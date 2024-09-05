0e197d61473cf981264be2cc34dea72ea2fadbb7
Alex Piet <alexpiet@gmail.com>
2024-09-05 10:48:58 -0700

    linting, more unit test coverage

--- tests/test_lick_annotation.py
+++ tests/test_lick_annotation.py
@@ -29,15 +29,18 @@ class TestLickAnnotation(unittest.TestCase):
 
         # Generate some simple data
         nwb = EmptyNWB()
-        times = [1, 1.2, 1.4, 5, 5.2, 10,20,20.2,20.4]
+        times = [1, 1.2, 1.4, 5, 5.2, 10, 20, 20.2, 20.4]
         df = pd.DataFrame(
             {
-                "timestamps": times,
-                "data": [1.0] * len(times),
-                "event": ["left_lick_time"] * 6 + ['right_lick_time']*3,
-                "trial": [1] * 6+[2] * 3,
+                "timestamps": times + [1.1, 20.1],
+                "data": [1.0] * (len(times) + 2),
+                "event": ["left_lick_time"] * 6
+                + ["right_lick_time"] * 3
+                + ["left_reward_delivery_time", "right_reward_delivery_time"],
+                "trial": [1] * 6 + [2] * 3 + [1, 2],
             }
         )
+        df = df.sort_values(by="timestamps")
 
         # Ensure the annotations run
         nwb.df_events = df
