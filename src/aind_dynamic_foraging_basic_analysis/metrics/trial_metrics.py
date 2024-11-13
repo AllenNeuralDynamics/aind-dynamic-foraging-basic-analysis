8f566540d0f8b65dba484f8ff1433ba364c8e8f7
Alex Piet <alexpiet@gmail.com>
2024-11-13 11:53:35 -0800

    adding choose right rate

--- src/aind_dynamic_foraging_basic_analysis/metrics/trial_metrics.py
+++ src/aind_dynamic_foraging_basic_analysis/metrics/trial_metrics.py
@@ -19,6 +19,9 @@ def compute_all_trial_metrics(nwb):
     gocue_reward_rate,      fraction of trials with a reward
     response_reward_rate,   fraction of trials with a reward,
                             computed only on trials with a response
+    choose_right_rate,      fraction of trials where chose right,
+                            computed only on trials with a response
+
     """
     if not hasattr(nwb, "df_trials"):
         print("You need to compute df_trials: nwb_utils.create_trials_df(nwb)")
@@ -45,8 +48,14 @@ def compute_all_trial_metrics(nwb):
         df["RESPONSE_REWARD"].rolling(WIN_DUR, min_periods=MIN_EVENTS, center=True).mean()
     )
 
+    # Rolling fraction of choosing right
+    df["WENT_RIGHT"] = [x if x in [0, 1] else np.nan for x in df["animal_response"]]
+    df["choose_right_rate"] = (
+        df["WENT_RIGHT"].rolling(WIN_DUR, min_periods=MIN_EVENTS, center=True).mean()
+    )
+
     # Clean up temp columns
-    drop_cols = ["RESPONDED", "RESPONSE_REWARD"]
+    drop_cols = ["RESPONDED", "RESPONSE_REWARD", "WENT_RIGHT"]
     df = df.drop(columns=drop_cols)
 
     return df
