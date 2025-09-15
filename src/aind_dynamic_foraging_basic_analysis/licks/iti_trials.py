6e7f6bc3713aea55c56b2538045d14d793e5fbb5
Alex Piet <alexpiet@gmail.com>
2025-09-15 11:54:50 -0700

    dev

---
+++ src/aind_dynamic_foraging_basic_analysis/licks/iti_trials.py
@@ -0,0 +1,55 @@
+"""
+Defines functions for analysis of intertrial licking as if they were trials
+
+"""
+import aind_dynamic_foraging_basic_analysis.licks.annotation as annotation
+
+def build_iti_trials_table(nwb):
+    """
+    TODO: fill in
+    """
+    if not hasattr(nwb, "df_trials"):
+        print("You need to compute df_trials: nwb_utils.create_df_trials(nwb)")
+        return
+
+    if not hasattr(nwb, "df_licks"):
+        print("You need to annotate df_licks: annotation.annotate_licks(nwb)")
+        return
+    if not 'bout_start' in nwb.df_licks:
+         print("You need to annotate df_licks: annotation.annotate_licks(nwb)")
+    if not 'bout_intertrial_choice' in nwb.df_licks:      
+          print("You need to annotate df_licks: annotation.annotate_licks(nwb)")
+   
+    iti_trials = nwb.df_licks.query('bout_start and bout_intertrial_choice').copy()
+    df_trials = nwb.df_trials.copy()
+    
+    # Adding trial information for iti_trials
+    iti_trials['animal_response'] = [0.0 if 'left' in x else 1.0 for x in iti_trials['event']]
+    iti_trials['bait_left'] = False
+    iti_trials['bait_right'] = False
+    iti_trials['reward_random_number_left'] = 0
+    iti_trials['reward_random_number_right'] = 0
+    iti_trials['auto_waterL']  = 0
+    iti_trials['auto_waterR'] = 0
+    iti_trials['laser_on_trial'] = 0
+    iti_trials['goCue_start_time_in_session'] = iti_trials['timestamps']
+    iti_trials['goCue_start_time_in_trial'] = 0
+    iti_trials['reward_outcome_time_in_session'] = iti_trials['timestamps']
+    iti_trials['reward_outcome_time_in_trial'] = 0
+    iti_trials['goCue_start_time_raw'] = iti_trials['raw_timestamps']
+    iti_trials['choice_time_in_session'] = iti_trials['timestamps']
+    iti_trials['choice_time_in_trial'] = 0
+    
+
+    # columns we need to recompute after merge
+    # rewarded_historyL, rewarded_historyR, 
+    
+    # columns we propagate after merge (all get NaNs now)
+    to_propagate = ['base_reward_probability_sum', 'reward_probabilityL', 'reward_probabilityR', 'left_valve_open_time', 'right_valve_open_time', 'block_beta', 'block_min', 'block_max', 'min_reward_each_block', 'delay_beta', 'delay_min', 'delay_max', 'response_duration', 'reward_consumption_duration', 'auto_train_engaged', 'auto_train_curriculum_name', 'auto_train_curriculum_version', 'auto_train_curriculum_schema_version', 'auto_train_stage', 'auto_train_stage_overridden', 'lickspout_position_x', 'lickspout_position_y', 'lickspout_position_z', 'reward_size_left', 'reward_size_right', 'ses_idx'] 
+
+    
+    # columns that will be undefined (get NaNs now)
+    stay_nans = ['laser_on_trial', 'laser_wavelength', 'laser_location', 'laser_power', 'laser_duration', 'laser_condition', 'laser_condition_probability', 'laser_start', 'laser_start_offset', 'laser_end', 'laser_end_offset', 'laser_protocol', 'laser_frequency', 'laser_rampingdown', 'laser_pulse_duration', 'bonsai_start_time_in_session', 'bonsai_start_time_in_trial', 'bonsai_stop_time_in_session', 'bonsai_stop_time_in_trial', 'delay_start_time_in_session', 'delay_start_time_in_trial', 'reward_time_in_session', 'reward_time_in_trial', 'right_reward_type', 'left_reward_type', 'earned_reward', 'extra_reward']
+
+    return iti_trials, df_trials 
+# dev
